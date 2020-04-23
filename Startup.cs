347ce35d205e1f14b9bd499f1303429a3ad2c705
Cezary Boguszewski <s18823@pjwstk.edu.pl>
2020-04-23 05:03:38 +0200

    cw6

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using cw4.Middlewares;
 namespace APBcw4
 {
     public class Startup
@@ -23,17 +24,37 @@ namespace APBcw4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+             app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+
+                if (!new StudentService().StudentExists(context.Request.Headers["Index"]))
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+
+                await next();
+
+                });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
-
+            app.UseMiddleware<LoggingMiddleware>();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
     }
-}
+ }
